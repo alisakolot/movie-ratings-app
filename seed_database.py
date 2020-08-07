55d300f451e7fc3eb496286066503cb44cb0c7bc
Alisa Kolot <kolot.alisa@gmail.com>
2020-08-06 23:00:51 -0500

    adding seed database

--- seed_database.py
+++ seed_database.py
@@ -9,13 +9,26 @@ import crud
 import model
 import server
 
-os.system('dropdb ratings')
-# More code will go here
 
 os.system('dropdb ratings')
 os.system('createdb ratings')
 
+model.connect_to_db(server.app)
+model.db.create_all()
 
-with.open('data/movies.json') as f:
+# Load movie data from JSON file
+with open('data/movies.json') as f:
     movie_data = json.loads(f.read())
-    
+
+# Create movies, store them in list so we can use them
+# to create fake ratings later
+movies_in_db = []
+for movie in movie_data: 
+    title, overview, poster_path (movie) = (movie['title'], movie['overview'], movie['poster_path'], 
+                                            movie[''])
+    release_date = datetime.strptime(movie['release-date'], '%Y-%m-%d')
+
+    db_movie = crud.create_movie(title, overview, release_date, poster_path)
+    movies_in_db.append(db_movie)
+    # TODO: create a movie here and append it to movies_in_db
+
